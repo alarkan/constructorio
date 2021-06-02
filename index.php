cdeeb00b28035dc729a4374fdd3e338e53342206
alarkan <jhon-ed113@hotmail.com>
2021-06-02 10:28:34 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,423 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8"/>
+	<title>Constructorio</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+	<style>
+	body{ font-family: 'Century Gothic'; margin: 0; padding: 0 }
+	#banner{ background-image: url('img/fondo-banner-3.jpg');background-size:cover;height: 100vh;background-position: center}
+	#services{ background-image: url('img/fondo-azul.jpg'); background-size: cover; background-position: center}
+	.nav-link img{ width: 120px }
+	#last-works{ background-color: #F1BD02 }
+	#video{ background-image: url('img/fondo-video.jpg'); background-size: cover; background-position: center }
+	.video-container{ position: relative; padding-bottom: 50%; padding-top: 30px;  height: 0; overflow: hidden; }
+	.video-container iframe,
+	.video-container object,
+	.video-container embed{ position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
+	.bg-smoke{ background-color: #f3f5f9 !important }
+	a.bg-smoke{text-transform: none !important;text-decoration: none !important;color: inherit !important;height: 100% !important}
+	.icon-fluid{ max-width: 100px !important; max-height: 100px !important }
+</style>
+</head>
+<body>
+	<div class="container-fluid" id="banner">
+		<div class="row">
+			<div class="col">
+				<nav class="navbar navbar-expand-lg navbar-light bg-gradient-secondary pt-3">
+					<button class="navbar-toggler bg-warning" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav text-center">
+							<li class="nav-item mr-3 mt-lg-0 mt-3">
+								<a class="nav-link scroll" href="#services">
+									<img src="img/boton-nav-inicio.png">
+								</a>
+							</li>
+							<li class="nav-item mr-3 mt-lg-0 mt-3">
+								<a class="nav-link scroll" href="#register">
+									<img src="img/boton-nav-registro.png">
+								</a>
+							</li>
+							<li class="nav-item mr-3 mt-lg-0 mt-3">
+								<a class="nav-link" href="#banner">
+									<img src="img/boton-nav-agendate.png">
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+				<div class="col">
+					<img src="img/logo-constructorio.png" class="img-fluid">
+				</div>
+			</div>
+
+			<div class="row ml-5 pt-5">
+				<div class="col-md-4 text-center">
+					<img src="img/aqui-encuentras-todo.png" class="img-fluid">
+				</div>
+			</div>
+		</div>
+
+		<div class="container-fluid px-0" id="services">
+			<div class="container py-5">
+				<div class="row">
+					<?php 
+					$categorias = array("Remodelacion","Reparacion Industria","Arquitecto","Cerrajeria","Cubiertas y Tejados","Dibujo Tecnico","Dibujo profesional","Diseño de Interiores","Electricos","Enchapes","Estructura para Cubiertas","Estuco","Mantenimiento y Reparaciones","Pinturas","Plomeria");
+					for ($i=0; $i < 15; $i++):
+						?>
+						<div class="col-4 p-2">
+							<a href="perfil.php?category=<?php echo $i+1;?>" class="btn btn-block bg-smoke">
+								<div class="row">
+									<div class="col-md-3">
+										<img src="img/categorias/<?php echo $i ?>.png" class="img-fluid">
+									</div>
+									<div class="col text-left pt-3 pr-2">
+										<span class="h5"><?php echo $categorias[$i];?></span>
+									</div>
+								</div>
+							</a>
+						</div>
+					<?php endfor ?>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid bg-white">
+		<div class="container" id="register">
+			<div class="row py-5">
+				<div class="col-md-6 pr-5">
+					<img src="img/se-parte-de-nuestro-constructorio.png" class="img-fluid">
+				</div>
+				<div class="col-md-6">
+					<form method="POST" action="index.php">
+						<div class="row my-3">
+							<div class="col-xs-1 pt-1 d-none d-md-block">
+								<span class="fas fa-key"></span>
+							</div>
+							<div class="col-md-11"><input required type="text" class="form-control" id="cedula" name="cedula" placeholder="N. Identificación"></div>
+						</div>
+						<div class="row my-3">
+							<div class="col-xs-1 pt-1 d-none d-md-block">
+								<span class="fas fa-user"></span>
+							</div>
+							<div class="col-md-5"><input required type="text" class="form-control" id="nombres" name="nombres" placeholder="Nombres"></div>
+							<div class="col-md-6"><input required type="text" class="form-control" id="apellidos" name="apellidos" placeholder="Apellidos"></div>
+						</div>
+						<div class="row mb-3">
+							<div class="col-md-6">
+								<div class="row">
+									<div class="col-xs-1 pt-1 d-none d-md-block">
+										<span class="fas fa-envelope-open"></span>
+									</div>
+									<div class="col-md-10">
+										<input required type="email" class="form-control" id="correo" name="correo" placeholder="Correo">
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="row">
+									<div class="col-xs-1 pt-1 d-none d-md-block">
+										<span class="fas fa-phone"></span>
+									</div>
+									<div class="col-md-10">
+										<input required type="text" class="form-control" id="telefono" name="telefono" placeholder="Teléfono">
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row mb-3">
+							<div class="col-md-6">
+								<div class="row">
+									<div class="col-xs-1 pt-1 d-none d-md-block">
+										<span class="fas fa-hammer"></span>
+									</div>
+									<div class="col-md-10">
+										
+										<select required class="form-control" id="ocupacion" name="ocupacion">
+											<option value="">Ocupación</option>
+											<?php for ($i=0; $i < 15; $i++): ?>
+												<option value="<?php echo $i+1;?>"><?php echo $categorias[$i];?></option>
+											<?php endfor ?>
+										</select>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="row">
+									<div class="col-xs-1 pt-1 d-none d-md-block">
+										<span class="fas fa-map-marker"></span>
+									</div>
+									<div class="col-md-10">
+										<select required class="form-control" id="ciudad" name="ciudad">
+											<option value="">Ciudad</option>
+											<option value="Buga">Buga</option>
+											<option value="Palmira">Palmira</option>
+											<option value="Tulua">Tuluá</option>
+										</select>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row mb-3">
+							<div class="col-xs-1 pt-1 d-none d-md-block">
+								<span class="fas fa-eye"></span>
+							</div>
+							<div class="col-md-11">
+								<textarea required class="form-control" id="especialidad" name="especialidad" placeholder="Especialidad"></textarea>
+							</div>
+						</div>
+						<div class="row justify-content-end">
+							<div class="col-md-4 mr-4"><button class="btn btn-warning btn-block">Enviar</button></div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="py-5 text-center full" id="last-works">
+	<h1 class="text-center text-dark">
+		<strong style="font-weight: 800">
+			Proveemos a nuestros clientes <br> Materiales para la construcción
+		</strong>
+	</h1>
+</div>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+		<li data-target="#myCarousel" data-slide-to="3"></li>
+	</ol>
+	<div class="carousel-inner" role="listbox">
+		<div class="carousel-item active">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_1.jpeg">
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_2.jpeg">
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_3.jpeg">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_4.jpeg">
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_5.jpeg">
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_6.jpeg">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_7.jpeg">
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_8.jpeg">
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_9.jpeg">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_10.jpeg">
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_11.jpeg">
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img" src="img/carousel_12.jpeg">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="sr-only"></span>
+	</a>
+	<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="sr-only"></span>
+	</a>
+</div>
+
+<div class="container-fluid" id="video">
+	<div class="row justify-content-center p-5">
+		<div class="col-md-7">
+			<div class="video-container sombra img-rounded">
+				<iframe src="https://www.youtube.com/embed/44MCEFWyRrE" frameborder="0" width="560" height="315" allowfullscreen></iframe>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid">
+	<div id="myCarousel2" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel2" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel2" data-slide-to="1"></li>
+			<li data-target="#myCarousel2" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner" role="listbox">
+			<div class="carousel-item active">
+				<div class="row">
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-1.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-2.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-3.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-4.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-5.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-6.png">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<div class="row">
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-7.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-8.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-9.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-10.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-11.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-12.png">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<div class="row">
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-13.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-10.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-15.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-16.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-17.png">
+						</div>
+					</div>
+					<div class="col-md-2">
+						<div class="card">
+							<img class="card-img" src="img/logo-carousel-12.png">
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<a class="carousel-control-prev" href="#myCarousel2" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only"></span>
+		</a>
+		<a class="carousel-control-next" href="#myCarousel2" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only"></span>
+		</a>
+	</div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ perfil.php
@@ -0,0 +1,160 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8"/>
+	<title>Constructorio</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+	<style>
+	@font-face{
+		font-family: 'Century Gothic';
+	}
+	body{
+		font-family: 'Century Gothic';
+		background-image: url('img/fondo-azul.jpg');
+		background-attachment: fixed;
+		background-size: cover
+	}
+	.text-primary{ color: steelblue !important }
+	.text-gold{ color: #F1BD02 !important }
+	.bg-dark{
+		background-size: cover; background-repeat: no-repeat;
+		height: 100vh; padding: 25vh 15%; margin: 0 !important
+	}
+	.contenido{
+		height: 280px;
+	}
+	@media screen(max-width: 800px){
+		.modal-bg-dark{ padding: 25vh 5% }
+	}
+	.pointer{ cursor: pointer }
+	.checked{ color: orange }
+</style>
+</head>
+<body>
+	<main id="app">
+		<?php if ($_GET): $_SESSION['categoria']= $_GET['category'];?>
+			<section class="container-fluid p-0">
+				<section class="row py-sm-5 px-5 bg-dark">
+					<div class="col-12 pb-5 text-center">
+						<a href="index.php"><h3 class="text-gold"><strong>ESTAMOS EN</strong></h3></a>
+					</div>
+					<form class="col-md-4 px-lg-5 pb-2" method="POST" action="perfil.php">
+						<input type="hidden" name="ciudad" value="buga"/>
+						<button class="bg-transparent" type="submit"><img src="img/boton-buga.png" class="img-fluid"></button>
+					</form>
+					<form class="col-md-4 px-lg-5 pb-2" method="POST" action="perfil.php">
+						<input type="hidden" name="ciudad" value="palmira"/>
+						<button class="bg-transparent" type="submit"><img src="img/boton-palmira.png" class="img-fluid"></button>
+					</form>
+					<form class="col-md-4 px-lg-5 pb-2" method="POST" action="perfil.php">
+						<input type="hidden" name="ciudad" value="tulua"/>
+						<button class="bg-transparent" type="submit"><img src="img/boton-tulua.png" class="img-fluid"></button>
+					</form>
+			</section>
+		</section>
+	<?php endif ?>
+	<?php if($_POST):?>
+		<section class="container-fluid px-5">
+			<section class="row justify-content-center" id="cabecera">
+				<div class="col-md-6 text-center mb-1 mt-0s">
+					<a href="index.php"><img src="img/logo-constructorio.png" class="img-fluid px-5"></a>
+					<p class="h4 text-white mt-3">¡AQUÍ LO ENCUENTRAS TODO!</p>
+				</div>
+			</section>
+			<section class="row pt-5 justify-content-center" id="contenido">
+				<div class="col-md-6 px-3">
+					<div class="row pl-3">
+						<div class="col-md-4">
+							<img src="img/profile-default.png" class="img-fluid" style="border-radius: 25px 0 25px 0">
+						</div>
+						<div class="col-md-8">
+							<div id="stars">
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span><br>
+								<span class="text-gold">100 estrellas</span>
+							</div>
+							<h1 class="text-white py-2">Nombre completo</h1>
+							<p class="text-white py-1">Descripcion</p>
+							<ul class="text-white">
+								<li class="text-muted">Email: <strong class="text-white">Correo</strong></li>
+								<li class="text-muted">Profesión: <strong class="text-white">Profesion</strong></li>
+								<li class="text-muted">Especialidad: <strong class="text-white">Especialidad</strong></li>
+								<li class="text-muted">Ciudad: <strong class="text-white">Ciudad</strong></li>
+							</ul>
+							<a href="https://wa.me/57" class="btn btn-outline-success">Contactar &nbsp; <i class="fab fa-whatsapp"></i></a>
+						</div>
+					</div>
+					<div class="row pl-3">
+						<div class="col-md-12">
+							<div class="row">
+								<div class="col-md-12 mb-3 text-center pt-3">
+									<span class="text-white h3">Conoce mi trabajo</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="row my-5 justify-content-center" id="similares">
+				<div class="col-sm-4 col-md-2 card rounded p-0 text-center mx-2 mt-3">
+					<picture style="background-image: url('img/profile-default.png'); background-size: cover; height: 12em;'"></picture>
+					<div class="p-2">
+						<div id="stars">
+							<span class="fa fa-star checked"></span>
+							<span class="fa fa-star"></span>
+							<span class="fa fa-star"></span>
+							<span class="fa fa-star"></span>
+							<span class="fa fa-star"></span>
+							<br><span class="text-gold">100 estrellas</span>
+						</div>
+						<a>
+							<strong class="text-primary">Nombre Cliente</strong><br>
+						</a>
+						<span>Correo Cliente</span><br>
+						<span>Telefono Cliente</span>
+						<a :href="https://wa.me/57" class="btn btn-outline-success"><i class="fab fa-whatsapp"></i></a>
+					</div>
+				</div>
+			</section>
+			<section class="row bg-white p-3 mb-5" id="comentarios">
+				<div class="col-12 py-2 px-3">
+					<div class="row">
+						<div class="col-6">
+							<input type="text" class="form-control" placeholder="Tu nombre" required>
+						</div>
+						<div class="col-6">
+							<input type="email" class="form-control" placeholder="Tu correo" required>
+						</div>
+					</div>
+					<div class="row pt-2">
+						<div class="col-10">
+							<input type="text" class="form-control" placeholder="Deja tu comentario">
+						</div>
+						<div class="col-2 text-right">
+							<button type="button" class="btn btn-primary">Enviar</button>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 py-2 px-3">
+					<ul>
+						<li>
+							<h3>Descripcion Comentario</h3>
+							<h6 class="text-primary">
+								<span>Nombre de comentarios</span>,
+								<strong>Correo de comentarios</strong>
+							</h6>
+						</li>
+					</ul>
+				</div>
+			</section>
+		</section>
+	<?php endif ?>
+</main>
+</body>
+</html>
